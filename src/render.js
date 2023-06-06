6d202212ed94ec494a08664c387fba01955fc5e4
Anna <yuferova.anna@yandex.ru>
2023-06-06 15:00:32 +0500

    1.10. Время разбивать камни

--- src/render.js
+++ src/render.js
@@ -1,13 +1,13 @@
 const RenderPosition = {BEFOREBEGIN: 'beforebegin', AFTERBEGIN: 'afterbegin', BEFOREEND: 'beforeend', AFTEREND: 'afterend'};
   
-  const createElement = (template) => {
+const createElement = (template) => {
     const newElement = document.createElement('div');
     newElement.innerHTML = template;
     return newElement.firstElementChild;
-  };
+};
   
-  const render = (component, container, place = RenderPosition.BEFOREEND) => {
+const render = (component, container, place = RenderPosition.BEFOREEND) => {
     container.insertAdjacentElement(place, component.getElement());
-  };
-  
-  export { RenderPosition, createElement, render };
+};
+
+export { RenderPosition, createElement, render };
