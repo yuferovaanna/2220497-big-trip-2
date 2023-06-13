5c3263ea68cff0029e5992e1d4b8ede10cf6c976
Anna <yuferova.anna@yandex.ru>
2023-06-13 23:54:18 +0500

    4.17. Шаблонизируй то

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { RenderPosition, render } from './framework/render.js';
-import PointsModel from './model/point-model.js';
-import PointFilterView from './view/filters.js';
-import RoutePresenter from './presenter/trip.js';
-import TripInfoView from './view/info-trip.js';
+import PointsModel from './model/points-model.js';
+import PointFilterView from './view/filter-view.js';
+import RoutePresenter from './presenter/route-presenter.js';
+import TripInfoView from './view/trip-info-view.js';
+import { generateFilter } from './mock/filter.js';
 
 const headerElement = document.querySelector('.page-header');
 const mainElement = document.querySelector('.page-body__page-main');
@@ -12,8 +13,9 @@ const tripEventsElement = mainElement.querySelector('.trip-events');
 
 const routePresenter = new RoutePresenter();
 const pointsModel = new PointsModel();
+const filters = generateFilter(pointsModel.points);
 
 render(new TripInfoView(), tripMainElement, RenderPosition.AFTERBEGIN);
-render(new PointFilterView(), tripControlsElement);
+render(new PointFilterView(filters), tripControlsElement);
 
 routePresenter.init(tripEventsElement, pointsModel);
---
+++ src/mock/filter.js
@@ -0,0 +1,8 @@
+import { filter } from '../utils/filter';
+
+export const generateFilter = (points) => Object.entries(filter).map(
+  ([filterName, filterPoints]) => ({
+    name: filterName,
+    count: filterPoints(points).length,
+  }),
+);
