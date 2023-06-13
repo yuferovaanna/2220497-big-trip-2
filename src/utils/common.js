1cc41560960a8ad8aac66d4e64457a9b45789a92
Anna <yuferova.anna@yandex.ru>
2023-06-13 23:25:56 +0500

    4.16. Разделяй и властвуй

--- src/utils/common.js
+++ src/utils/common.js
@@ -3,5 +3,4 @@ const getRandomInteger = (a = 0, b = 1) => {
   const upper = Math.floor(Math.max(a, b));
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
-  
 export { getRandomInteger };
